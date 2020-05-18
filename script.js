559ab6388c060d25c433391c4d9bb31be17a9c61
unknown <paweladamian@gmail.com>
2020-05-18 20:45:46 +0200

    Change typography

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ $(function () {
     
     $(window).on('load', function() {
        $('#preloader #image').fadeOut();
-        $('#preloader').delay(50).fadeOut('slow');
+        $('#preloader').delay(30).fadeOut('slow');
     });
     
     $('a[href^="#"]').on('click', function (event) {
@@ -27,40 +27,30 @@ $(function () {
         if ($(this).scrollTop() > $('.horizontal-first').offset().top){
             $('.aboutUs .container').addClass('show');
         } 
-        // else $('.offer .container').removeClass('show');
     })
 
     // animacja sekcji miniGallery
     $(window).on('scroll', function() {
         if ($(this).scrollTop() > $('.horizontal-second').offset().top){
-            // if ($(this).scrollTop() > $('.mini-gallery .container .row:nth-of-type(4)').offset().top){
                 $('.gallery-link .container').addClass('show-gallery');
         } 
-        // else $('.mini-gallery [class*="image-"]').removeClass('show');
     })
     
 
     // animacja sekcji stolarnie
     $(window).on('scroll', function() {
         if ($(this).scrollTop() > $('.horizontal-third').offset().top){
-        // if ($(this).scrollTop() > $('.mini-gallery .container .row:nth-of-type(4)').offset().top){
             $('.offer .container').addClass('show');
         } 
-        // else $('.aboutUs .container').removeClass('show');
     })
     
 
     // animacja sekcji kontakt
     $(window).on('scroll', function() {
         if ($(this).scrollTop() > $('.horizontal-fourth').offset().top){
-        // if ($(this).scrollTop() > $('.aboutUs .container p').offset().top){
             $('.contact .container').addClass('show');
             $('.contact iframe').addClass('show');
         } 
-        // else {
-        //     $('.contact .container').removeClass('show');
-        //     $('.contact iframe').removeClass('show');
-        // }
     })
 
     
